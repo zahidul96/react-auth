43b5266adca818f21eae73bc02f6156a7e891dfb
zahidul96 <zahidulhussain111@gmail.com>
2026-06-17 20:40:31 +0600

    first commit

---
+++ react-auth/src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router";
+
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+
+const App = () => {
+  return (
+   <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/profile" element={<UserProfile />} />
+      </Routes>
+    </Layout>
+  );
+};
+
+export default App;
